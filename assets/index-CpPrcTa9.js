55e3810094f5eeab5cd351d559b029b93ddfe420
egenerse <egenerse@gmail.com>
2024-06-02 11:13:49 +0200

    Updates

--- assets/index-CpPrcTa9.js
+++ assets/index-CpPrcTa9.js
@@ -43,7 +43,6 @@ Error generating stack: `+o.message+`
   width: 100%;
   background: #e77650;
   opacity: 1;
-  padding: 0.5rem;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: column;
@@ -52,10 +51,11 @@ Error generating stack: `+o.message+`
   transition: transform 0s ease;
   z-index: ${bu.navbar};
 `,Ad=25,Mk=()=>{const[e,t]=E.useState(!0),[n,r]=E.useState(0),[i,o]=E.useState(0),[s,l]=E.useState(0),a=()=>{const u=window.scrollY;u>n?(l(c=>c+(u-n)),s>Ad&&t(!1),o(0)):(o(c=>c+(n-u)),i>Ad&&t(!0),l(0)),r(u)};return E.useEffect(()=>(window.addEventListener("scroll",a),()=>{window.removeEventListener("scroll",a)}),[n,i]),L.jsx(Lk,{initial:{opacity:1},animate:{opacity:e?1:0,y:e?0:-100},children:L.jsx("h1",{children:"Ege Nerse"})})},Dk=Q.div`
-  margin-top: 3rem;
+  margin: 2rem;
+  margin-top: 5rem;
 `,Vk=Q.div`
   font-weight: bold;
-`;function Nk(){return L.jsxs(Dk,{children:[L.jsxs(Vk,{children:["Munich, Germany —"," ",L.jsx("a",{href:"mailto:egenerse@gmail.com",children:"egenerse@gmail.com"})," — (+49) 157 34886998"]}),L.jsxs("p",{children:[L.jsx("a",{href:"https://www.linkedin.com/in/ege-nerse-b81aba115/",target:"_blank",children:"LinkedIn"})," ","—"," ",L.jsx("a",{href:"https://github.com/egenerse",target:"_blank",children:"Github"})]})]})}const Ok=Q(Yu.button)`
+`;function Nk(){return L.jsxs(Dk,{children:[L.jsxs(Vk,{children:["Munich, Germany —"," ",L.jsx("a",{href:"mailto:egenerse@gmail.com",children:"egenerse@gmail.com"})," — (+49) 157 34886998"]}),L.jsxs("div",{children:[L.jsx("a",{href:"https://www.linkedin.com/in/ege-nerse-b81aba115/",target:"_blank",children:"LinkedIn"})," ","—"," ",L.jsx("a",{href:"https://github.com/egenerse",target:"_blank",children:"Github"})]})]})}const Ok=Q(Yu.button)`
   margin-top: 1rem;
   padding: 0.5rem 1rem;
   font-size: 1rem;
@@ -151,7 +151,6 @@ Error generating stack: `+o.message+`
   gap: 1rem;
   width: 90%;
   max-width: 900px;
-  margin-top: 2rem;
 `,sP=()=>L.jsxs(oP,{children:[L.jsx(Cr,{title:"Working Student",subtitle:"MVST, Munich, Germany (11/2023 - Present)",content:["Developing a production-ready React Native application from scratch with a 5-member team.","Implementing core components, screens, and API integrations, ensuring high performance and usability.","Coordinating with clients and team members to align project goals and deliverables."],logo:eP}),L.jsx(Cr,{title:"Working Student",subtitle:"ABB, Munich, Germany (04/2023 - 09/2023)",content:["Collaborated with the R&D Manager to enhance the internal user experience within Polarion.","Developed a custom workflow using JavaScript to automate the creation of user accounts and assigning roles in projects by dynamically checking Active Directory tables.","Created reusable project templates to enhance efficiency in project management."],logo:tP}),L.jsx(Cr,{title:"Full-Time React Native Contractor Mobile Developer",subtitle:"StartupHeroes & GoPuff, Remote, Istanbul, USA (09/2021 – 09/2022)",content:["Proficient in cross-platform mobile frontend development.","Developed an application for the delivery of consumer goods and food using React Native, TypeScript, and GraphQL for GoPuff, collaborating with an international team.","Took ownership of pages, components, and user flows, ensuring their high-quality development and carefully reviewing all changes for accuracy and efficiency."],logo:nP}),L.jsx(Cr,{title:"Full-Time React Native Mobile Developer",subtitle:"Medyasoft Inc, Ankara, Turkey (03/2021 – 08/2021)",content:["Proficient in project development and mobile app development using React Native.","Actively contributed to the development of mobile pages and components for ”Kizilay Mobil” and ”Kizilay GYS” applications, benefiting The Turkish Red Crescent and its volunteers.","Engaged in regular meetings with The Turkish Red Crescent team, presenting development progress and updating them on the latest enhancements."],logo:rP}),L.jsx(Cr,{title:"Internship, Part-Time, Full-Time Backend Developer",subtitle:"Hesapkurdu.com Inc, Remote, Turkey (07/2020 – 02/2021)",content:["Resolved system bugs, SEO enhancements for improved online visibility, enhanced user experience, and also improved content management system.","Developed new controllers and services, enhancing user interactions.","Developed a new scheduled job to monitor real-time fuel price and optimized it with Redis cache."],logo:iP})]}),lP=Q.div`
   display: flex;
   flex-direction: column;
